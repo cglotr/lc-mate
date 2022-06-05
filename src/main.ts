8575ead1251059eb0f09341a5f6014c63d106efc
cglotr <amir.ariffin.1992@gmail.com>
2022-06-06 03:05:09 +0800

    test main?

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { ApiImpl } from './api/api_impl'
 import { UserServiceImpl } from './service/user_service_impl'
 import { WorkerImpl } from './worker_impl'
 
-function main() {
+export function main() {
   const apiImpl = new ApiImpl()
   const userService = new UserServiceImpl(apiImpl)
   const worker = new WorkerImpl(document as any, userService)
---
+++ src/main_test.ts
@@ -0,0 +1,7 @@
+import { main } from './main'
+
+describe('main', () => {
+  test('?', () => {
+    main()
+  })
+})
